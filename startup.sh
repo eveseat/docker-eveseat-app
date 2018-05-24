e7bb2dd24d13c680f1662438234d45b15c0ab13c
Leon Jacobs <leonja511@gmail.com>
2018-05-24 18:57:00 +0200

    Move permissions fixing logic after plugin installs.

--- startup.sh
+++ startup.sh
@@ -6,9 +6,6 @@ set -e
 find . -maxdepth 1 ! -name . -exec rm -r {} \; && \
    tar cf - --one-file-system -C /usr/src/seat . | tar xf -
 
-# Fix up permissions
-chown -R www-data:www-data storage
-
 # Wait for the database
 while ! mysqladmin ping -hmariadb --silent; do
 
@@ -59,5 +56,11 @@ if [ ! "$plugins" == "" ]; then
 fi
 
 echo "Completed plugins processing"
+
+# Fix up permissions
+chown -R www-data:www-data storage
+chown -R www-data:www-data .
+find . -type d -print0 | xargs -0 chmod 775
+find . -type f -print0 | xargs -0 chmod 664
 
 php-fpm -F
